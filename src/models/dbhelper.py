f81a2362c52ed16f3e3895027e7800129eecef30
redun <redunsasha2017@gmail.com>
2026-02-19 17:29:45 +0700

    add methods for tests

--- src/models/dbhelper.py
+++ src/models/dbhelper.py
@@ -1,9 +1,12 @@
 from asyncio import current_task
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import (
     async_scoped_session,
     async_sessionmaker,
     create_async_engine,
+    AsyncSession,
 )
 
 from src.config import settings
@@ -38,6 +41,23 @@ class DatabaseHelper:
         session = self.get_scoped_session()
         yield session
         await session.close()
+
+    async def dispose(self):
+        """Разрыв."""
+        await self.engine.dispose()
+
+    @asynccontextmanager
+    async def session(self) -> AsyncGenerator[AsyncSession, None]:
+        """Контекстный менеджер для получения сессии"""
+        async with self.session_factory() as session:
+            try:
+                yield session
+                await session.commit()
+            except Exception:
+                await session.rollback()
+                raise
+            finally:
+                await session.close()
 
 
 db_helper = DatabaseHelper(url=settings.db.url, echo=settings.db.echo)
